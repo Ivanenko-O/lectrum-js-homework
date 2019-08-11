6ce39a1070fae9ae30e624483213e999cbfc9086
O-Ivanenko <oles.ivanenko@gmail.com>
2019-08-11 21:07:06 +0300

    Add tasks related closure

---
+++ lesson-11/2.js
@@ -0,0 +1,42 @@
+/**
+ * Задача 2.
+ *
+ * Создайте функцию createFibonacciGenerator(),
+ * которая вернёт ещё одну функцию,
+ * каждый вызов которой будет возвращать число из последовательности Фибоначчи.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+function createFibonacciGenerator() {
+	let a = 0;
+	let b = 1;
+
+	return function() {
+		let sum = a + b;
+
+		if (a === 0) {
+			a = b;
+			return sum;
+		}
+
+		a = b;
+		b = sum;
+
+		return b;
+	};
+}
+
+const generateFibonacciNumber = createFibonacciGenerator();
+
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 2
+console.log(generateFibonacciNumber()); // 3
+console.log(generateFibonacciNumber()); // 5
+console.log(generateFibonacciNumber()); // 8
+console.log(generateFibonacciNumber()); // 13
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
